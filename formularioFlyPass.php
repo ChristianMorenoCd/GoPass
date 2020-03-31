7add351796fb2cf136b97ce2d4a596078a13f149
ChristianMorenoCd <christianm.code@gmail.com>
2020-03-31 11:48:07 -0500

    Se arreglan vista de pagina de vinculacion, a falta de agregar propiedades responsive, se valida que el servicio este funcionando correctamente

--- formularioFlyPass.php
+++ formularioFlyPass.php
@@ -6,12 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="css/desvinculacionFlyPass.css">
+   
+
     <title>Desvinculate</title>
 </head>
 
 <body class="home-body">
     <header>
         <div>
+        <div class="imagen-container-gopass">
+            <a href=""><img src="assets/img//desviculacionFlyPassImg/Logo-gopass-desvinculate.png" alt=""></a>
+        </div>
         <div class="imagen-container">
             <img src="assets/img/desviculacionFlyPassImg/imagen-desvinculacion-flypass.png" alt="">
         </div>
