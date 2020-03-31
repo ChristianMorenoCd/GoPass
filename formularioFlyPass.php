fd688ddd25f566b1be52fc1aae3325944c1018d7
ChristianMorenoCd <christianm.code@gmail.com>
2020-03-31 00:33:17 -0500

    Se agrega vista para consumo de API, se agregan propiedades responsive faltantes

--- formularioFlyPass.php
+++ formularioFlyPass.php
@@ -4,18 +4,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <link rel="stylesheet" href="css/desvinculacionFlyPass.css">
+    <title>Desvinculate</title>
 </head>
 
-<body>
-    <h1>Formulario</h1>
-
-    <form action="desvinculacionFlyPass.php" method="post">
-        <p>cedula<input type="text" name="cedula" /></p>
-        <p>pass<input type="text" name="pass" /></p>
-        <p>placa<input type="text" name="placa" /></p>
-        <p><input type="submit" /></p>
-    </form>
+<body class="home-body">
+    <header>
+        <div>
+        <div class="imagen-container">
+            <img src="assets/img/desviculacionFlyPassImg/imagen-desvinculacion-flypass.png" alt="">
+        </div>
+        <div class="container-form-desvinculacion-flypass">
+            <form action="desvinculacionFlyPass.php" method="post" required>
+                <input type="text" name="cedula" placeholder="Cedula" required>
+                <br>
+                <input type="password" name="pass"  placeholder="Contraseña" required>
+                <p>
+                    Placa del vehiculo que deseas desvincular
+                </p>
+                <input type="text" name="placa"  placeholder="Placa" required>
+                <br>
+                <input class="button-desvicular-flypass" type="submit" value="Desvincular" >
+            </form>
+        </div>
+        </div>
+    </header>
 </body>
 
 </html>
